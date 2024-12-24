3a4a14d11e30c88f65fc3a54065b81a1f859c17a
marianbasti <31198560+marianbasti@users.noreply.github.com>
2024-12-24 11:17:03 -0300

    [FIX] Use default factory for modalities and set shared_decoder to None in Data2VecMultiConfig

--- EAT/models/EAT_pretraining.py
+++ EAT/models/EAT_pretraining.py
@@ -119,9 +119,9 @@ class Data2VecMultiConfig(FairseqDataclass):
 
     max_update: int = II("optimization.max_update")
 
-    modalities: D2vModalitiesConfig = D2vModalitiesConfig()
+    modalities: D2vModalitiesConfig = field(default_factory=D2vModalitiesConfig)
 
-    shared_decoder: Optional[D2vDecoderConfig] = None
+    shared_decoder: Optional[D2vDecoderConfig] = field(default=None)
 
     min_target_var: float = field(
         default=0.1, metadata={"help": "stop training if target var falls below this"}
