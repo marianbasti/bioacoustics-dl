32393c68816f19cd71d285bea0fa603efdb04830
marianbasti <31198560+marianbasti@users.noreply.github.com>
2024-12-27 15:40:57 -0300

    [MOD] eval.sh to improve argument handling for evaluation parameters

--- EAT/scripts/eval.sh
+++ EAT/scripts/eval.sh
@@ -2,14 +2,22 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
+LABEL_FILE=${1:-"EAT/inference/labels.csv"}
+EVAL_DIR=${2:-"/hpc_stor03/sjtu_home/wenxi.chen/mydata/audio/AS20K"}
+MODEL_DIR=${3:-"EAT"}
+CHECKPOINT_DIR=${4:-"/hpc_stor03/sjtu_home/wenxi.chen/model_ckpt/EATs/finetuning/as20k_epoch30/checkpoint_last.pt"}
+TARGET_LENGTH=${5:-1024}
+DEVICE=${6:-"cuda"}
+BATCH_SIZE=${7:-32}
+
 python EAT/evaluation/eval.py  \
-    --label_file='EAT/inference/labels.csv' \
-    --eval_dir='/hpc_stor03/sjtu_home/wenxi.chen/mydata/audio/AS20K' \
-    --model_dir='EAT' \
-    --checkpoint_dir='/hpc_stor03/sjtu_home/wenxi.chen/model_ckpt/EATs/finetuning/as20k_epoch30/checkpoint_last.pt' \
-    --target_length=1024 \
-    --device='cuda' \
-    --batch_size=32
+    --label_file="$LABEL_FILE" \
+    --eval_dir="$EVAL_DIR" \
+    --model_dir="$MODEL_DIR" \
+    --checkpoint_dir="$CHECKPOINT_DIR" \
+    --target_length="$TARGET_LENGTH" \
+    --device="$DEVICE" \
+    --batch_size="$BATCH_SIZE"
 
 # For optimal performance, 1024 is recommended for 10-second audio clips. (128 for 1-second)
 # However, you should adjust the target_length parameter based on the duration and characteristics of your specific audio inputs.
